71629fea920c74a7aed79023b0c469c9c1e52c1d
Christian Pieringer <8143906+cpieringer@users.noreply.github.com>
2021-01-17 12:09:54 -0300

    update notebook and fix issues

--- listings/chapter_05/ex-5.8.py
+++ listings/chapter_05/ex-5.8.py
@@ -1,36 +1,36 @@
-# [INPUT]  X  : matrix of training features, one sample per row
-#          d  : vector of training labels
-#          Xt : matrix of testing features, one sample per row
-#          dt : vector of testing labels
-#          s  : string with the name of the model
-#          p  : number of features to be selected
-# [OUTPUT] q  : indices of selected features (columns)
-#          X  : new matrix of training features
-#          Xt : new matrix of testing features
+"""
+[INPUT]  X  : matrix of training features, one sample per row
+         d  : vector of training labels
+         Xt : matrix of testing features, one sample per row
+         dt : vector of testing labels
+         s  : string with the name of the model
+         p  : number of features to be selected
+[OUTPUT] q  : indices of selected features (columns)
+         X  : new matrix of training features
+         Xt : new matrix of testing features
+"""
 
+from sklearn.neighbors import KNeighborsClassifier as KNN
 from pyxvis.features.selection import fse_model, fsel
 from pyxvis.io.data import load_features
-from sklearn.neighbors import KNeighborsClassifier as KNN  
 from pyxvis.io.plots import print_confusion
 
 # Definition of input variables
-(X,d,Xt,dt) = load_features('../data/F40/F40')
+(X, d, Xt, dt) = load_features('../data/F40/F40')
 s = 'lda'
 p = 5
 
 # Feature selection
-(name,params) = fse_model(s)
-q = fsel([name,params],X,d,p,cv = 5, show = 1)
-print(str(len(q)) +' from ' + str(X.shape[1]) +' features selected.')
+(name, params) = fse_model(s)
+q = fsel([name, params], X, d, p, cv=5, show=1)
+print(str(len(q)) + ' from ' + str(X.shape[1]) + ' features selected.')
 
 # New training and testing data
-X = X[:,q]
-Xt = Xt[:,q]
+X = X[:, q]
+Xt = Xt[:, q]
 
 # Classification and Evaluation
 clf = KNN(n_neighbors=5)
 clf.fit(X, d)
 ds = clf.predict(Xt)
-print_confusion(dt,ds)
-
-
+print_confusion(dt, ds)
