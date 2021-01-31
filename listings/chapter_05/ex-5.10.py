d10f78575937c8a0c8ef2a4882ab035ec996b09e
Christian Pieringer <cppierin@uc.cl>
2021-01-31 18:32:39 -0300

    update opecv version

--- listings/chapter_05/ex-5.10.py
+++ listings/chapter_05/ex-5.10.py
@@ -1,33 +1,33 @@
-# ex_sfs_threatobjects.py: Example of extraction and selection of geometric features
+"""
+ex_sfs_threatobjects.py: Example of extraction and selection of geometric features.
+"""
 
-import numpy as np
-from pybalu.feature_selection import clean
+# import numpy as np
+# from pybalu.feature_selection import clean
 from pybalu.feature_transformation import normalize, pca
 from pyxvis.features.extraction import extract_features_labels
 from pyxvis.features.selection import fse_model, fse_sbs, clean_norm, clean_norm_transform
 from pyxvis.io.plots import plot_features3, print_confusion
-from sklearn.neighbors import KNeighborsClassifier as KNN  
+from sklearn.neighbors import KNeighborsClassifier as knn
 
 # Training-Data
-path          = '../images/threatobjects/'
-fx            = ['basicgeo','ellipse','hugeo','flusser','fourierdes','gupta']
-X,d           = extract_features_labels(fx,path+'train','jpg',segmentation = 'bimodal')
-X,sclean,a,b  = clean_norm(X)
-(name,params) = fse_model('LDA')
-ssbs          = fse_sbs([name,params],X,d,20)
-X             = X[:,ssbs]
-Ypca,_,_,_,_  = pca(X, n_components=3)
-plot_features3(Ypca,d,'PCA - Threat Objects',view=(-160,120))
+path = '../images/threatobjects/'
+fx = ['basicgeo', 'ellipse', 'hugeo', 'flusser', 'fourierdes', 'gupta']
+X, d = extract_features_labels(fx, path + 'train', 'jpg', segmentation='bimodal')
+X, sclean, a, b = clean_norm(X)
+(name, params) = fse_model('LDA')
+ssbs = fse_sbs([name, params], X, d, 20)
+X = X[:, ssbs]
+Ypca, _, _, _, _ = pca(X, n_components=3)
+plot_features3(Ypca, d, 'PCA - Threat Objects', view=(-160, 120))
 
 # Testing-Data
-Xt,dt         = extract_features_labels(fx,path+'test','jpg',segmentation = 'bimodal')
-Xt            = clean_norm_transform(Xt,sclean,a,b)
-Xt            = Xt[:,ssbs] 
+Xt, dt = extract_features_labels(fx, path + 'test', 'jpg', segmentation='bimodal')
+Xt = clean_norm_transform(Xt, sclean, a, b)
+Xt = Xt[:, ssbs]
 
 # Classification and Evaluation
-clf = KNN(n_neighbors=5)
+clf = knn(n_neighbors=5)
 clf.fit(X, d)
-ds  = clf.predict(Xt)
-print_confusion(dt,ds)
-
-
+ds = clf.predict(Xt)
+print_confusion(dt, ds)
--- listings/chapter_05/ex-5.9.py
+++ listings/chapter_05/ex-5.9.py
@@ -5,38 +5,34 @@ from pybalu.feature_analysis import jfisher
 from pyxvis.features.extraction import extract_features_labels
 from pyxvis.features.selection import fsel, fse_model, clean_norm, clean_norm_transform
 from pyxvis.io.plots import plot_features3, print_confusion
-from sklearn.neighbors import KNeighborsClassifier as KNN  
+from sklearn.neighbors import KNeighborsClassifier as KNN
+
 
 # Training-Data
-path          = '../images/fishbones/'
-fx            = ['basicint','gabor-ri','lbp-ri','haralick-2','fourier','dct','hog']
-X,d           = extract_features_labels(fx,path+'train','jpg')
-X,sclean,a,b  = clean_norm(X)
-(name,params) = fse_model('QDA')
-ssfs          = fsel([name,params],X,d,15,cv = 5, show = 1)
-X             = X[:,ssfs]
-Ypca,_,A,Mx,_ = pca(X, n_components=6)
-X             = np.concatenate((X,Ypca),axis=1)
-sf            = exsearch(X, d, n_features=3 ,method="fisher",show=True)
-X             = X[:,sf]
-print('Jfisher = ' + str(jfisher(X,d)))
-plot_features3(X,d,'Fishbones')
+path = '../images/fishbones/'
+fx = ['basicint', 'gabor-ri', 'lbp-ri', 'haralick-2', 'fourier', 'dct', 'hog']
+X, d = extract_features_labels(fx, path + 'train', 'jpg')
+X, sclean, a, b = clean_norm(X)
+(name, params) = fse_model('QDA')
+ssfs = fsel([name, params], X, d, 15, cv=5, show=1)
+X = X[:, ssfs]
+Ypca, _, A, Mx, _ = pca(X, n_components=6)
+X = np.concatenate((X, Ypca), axis=1)
+sf = exsearch(X, d, n_features=3, method="fisher", show=True)
+X = X[:, sf]
+print('Jfisher = ' + str(jfisher(X, d)))
+plot_features3(X, d, 'Fishbones')
 
 # Testing-Data
-Xt,dt         = extract_features_labels(fx,path+'test','jpg')
-Xt            = clean_norm_transform(Xt,sclean,a,b)
-Xt            = Xt[:,ssfs] 
-Ytpca         = np.matmul(Xt - Mx, A)
-Xt            = np.concatenate((Xt,Ytpca),axis=1)
-Xt            = Xt[:,sf]
+Xt, dt = extract_features_labels(fx, path + 'test', 'jpg')
+Xt = clean_norm_transform(Xt, sclean, a, b)
+Xt = Xt[:, ssfs]
+Ytpca = np.matmul(Xt - Mx, A)
+Xt = np.concatenate((Xt, Ytpca) , axis=1)
+Xt = Xt[:, sf]
 
 # Classification and Evaluation
-clf           = KNN(n_neighbors=5)
+clf = KNN(n_neighbors=5)
 clf.fit(X, d)
-ds            = clf.predict(Xt)
-print_confusion(dt,ds)
-
-
-
-
-
+ds = clf.predict(Xt)
+print_confusion(dt, ds)
